e6ce056556e79ab4748f94635d910c9b95257410
dugwill <douglaswi1@comcast.net>
2019-02-04 16:25:48 -0700

    Trying to make this work

--- replyToPost.go
+++ replyToPost.go
@@ -5,45 +5,41 @@ import (
 	"html/template"
 	"log"
 	"net/http"
-	"strings"
+)
+
+var t, _ = template.ParseFiles(
+	"index.html",
 )
 
 func main() {
-	http.HandleFunc("/", sayhelloName) // setting router rule
-	http.HandleFunc("/login", login)
+	http.HandleFunc("/index", index) // setting router rule
+
 	err := http.ListenAndServe(":9090", nil) // setting listening port
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
 }
 
-func login(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("method:", r.Method) //get request method
-	if r.Method == "GET" {
-		t, _ := template.ParseFiles("login.html")
-		t.Execute(w, nil)
-	} else {
-		r.ParseForm()
-		// logic part of log in
-		fmt.Println("username:", r.Form["username"])
-		fmt.Println("password:", r.Form["password"])
-	}
-}
+func index(w http.ResponseWriter, r *http.Request) {
 
-func sayhelloName(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm() //Parse url parameters passed, then parse the response packet for the POST body (request body)
-	// attention: If you do not call ParseForm method, the following data can not be obtained form
-	fmt.Println(r.Form) // print information on server side.
-	fmt.Println("path", r.URL.Path)
-	fmt.Println("scheme", r.URL.Scheme)
-	fmt.Println(r.Form["url_long"])
-	for k, v := range r.Form {
-		fmt.Println("key:", k)
-		fmt.Println("val:", strings.Join(v, ""))
+	data := struct {
+		Title  string
+		Header string
+		Name   string
+	}{
+		Title:  "Index page",
+		Header: "Index",
+		Name:   "Index Page",
 	}
-	//fmt.Fprintf(w, "Hello astaxie!") // write data to response
 
-	t, _ := template.ParseFiles("bob.html")
-	t.Execute(w, nil)
+	fmt.Println("index")
+
+	if err := t.ExecuteTemplate(w, "newEventHandle.html", data); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	//t, _ := template.ParseFiles("index.html")
+	//t.ExecuteTemplate(w, "index.html", data)
 
 }
